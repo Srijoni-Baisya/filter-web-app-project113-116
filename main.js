eae6ce2c1f8b355b17cb659d533dbf201d0a2892
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-06-20 11:03:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+//insert filter img link
+}
+function setup(){
+    canvas = createCanvas(550,550);
+    canvas.center();//store canvas in var and place it in center
+}
+
+function draw(){
+    //access webcam and apply filter
+}
+function take_snapshot(){
+    save('mustache_filter_image.png');//save img
+}
