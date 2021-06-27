08a43bfce87881ada529c50fa8d8beb6910f6883
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-06-27 12:32:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
 function preload(){
 //insert filter img link
 }
+
 function setup(){
     canvas = createCanvas(550,550);
     canvas.center();//store canvas in var and place it in center
+
+    //part1 - accessing webcam by createCapture() function
+    video = createCapture(VIDEO);
+    video.size(550,550);
+    video.hide(); //hiding extra component
+
+    //part3- initializing poseNet model using a predefined function
+    poseNet = ml5.poseNet(video,modelLoaded);
+
+    //part5 - executing poseNet model using on() function
+    poseNet.on('pose',gotPoses);
+}
+
+//part4 - define modelLoaded() function to check that poseNet model has been loaded
+function modelLoaded(){
+    console.log("poseNet model is initialized !");
+}
+
+//part6 - define gotPoses() function to get x and y coordinates of body parts and display nose x and nose y coordinates on console
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        console.log("Nose x coordinate = "+ results[0].pose.nose.x);
+        console.log("Nose y coordinate = "+ results[0].pose.nose.y);
+    }
 }
 
 function draw(){
-    //access webcam and apply filter
+    //part2 - load webcam on canvas
+    image(video,0,0,550,550);
 }
 function take_snapshot(){
     save('mustache_filter_image.png');//save img
